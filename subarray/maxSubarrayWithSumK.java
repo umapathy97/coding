992517652713c93187e63a480a68f5873c9be3e8
umapathy <36830805+umapathy97@users.noreply.github.com>
2020-08-09 13:20:11 +0530

    Create maxSubarrayWithSumK.java

---
+++ subarray/maxSubarrayWithSumK.java
@@ -0,0 +1,72 @@
+/**Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
+
+Example
+Example1
+
+Input:  nums = [1, -1, 5, -2, 3], k = 3
+Output: 4
+Explanation:
+because the subarray [1, -1, 5, -2] sums to 3 and is the longest.
+Example2
+
+Input: nums = [-2, -1, 2, 1], k = 1
+Output: 2
+Explanation:
+because the subarray [-1, 2] sums to 1 and is the longest.
+Challenge
+Can you do it in O(n) time?
+
+Notice
+The sum of the entire nums array is guaranteed to fit within the 32-bit signed integer range.
+
+
+**/
+
+public class Solution {
+    /**
+     * @param nums: an array
+     * @param k: a target value
+     * @return: the maximum length of a subarray that sums to k
+     */
+    public int maxSubArrayLen(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int maxLength = 0;
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                maxLength = Math.max(maxLength, i - map.get(sum - k));
+            }
+            else
+            map.putIfAbsent(sum,i);
+            
+        }
+        return maxLength;
+    }
+}
+
+/**
+ * 
+ * [1,-1,5,-2,3]
+3
+
+
+{  
+    1, 0
+    0, 1
+    5, 2
+    
+    
+}
+
+[-2,-1,2,1]
+{
+-2, 0
+-3, 1
+-1, 2
+
+
+}
+ * 
+ * **/
